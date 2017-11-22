79d92990f06dc7a49a025b2c52924222a45801aa
vbnin <31508339+vbnin@users.noreply.github.com>
2017-11-22 17:32:18 +0100

    changes

--- SNMPInfo/Libraries.py
+++ SNMPInfo/Libraries.py
@@ -8,11 +8,13 @@ Ce fichier est une librairie requise par le script core.py
 
 # Import des librairies
 import logging
+import re
+import csv
 from logging.handlers import RotatingFileHandler
 from pysnmp.hlapi import *
 
 # Activation du logger
-handler = RotatingFileHandler('/var/log/SNMPInfo.log', maxBytes=10000000, backupCount=5)
+handler = RotatingFileHandler('D:\SNMPInfo.log', maxBytes=10000000, backupCount=5)
 handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
 logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
 logger = logging.getLogger(__name__)
@@ -47,8 +49,22 @@ def SNMPget(IPAddr, OID):
             for varBind in varBinds:
                 state = (' = '.join([x.prettyPrint() for x in varBind]))
                 logger.debug(state)
+                m = re.search('(.*)\ =\ (.*)', state)
+                state = m.group(2)
                 return state
     except:
         logger.error("Erreur générale...")
         state = '2'
         return state
+
+def DR5000state(Addr, DR5000Model, DR5000Name, DR5000Snr, DR5000Margin, DR5000SvcName, c):
+    Info = {'Model':DR5000Model,
+            'Name':SNMPget(Addr, DR5000Name),
+            'Snr':SNMPget(Addr, DR5000Snr),
+            'Margin':SNMPget(Addr, DR5000Margin),
+            'SvcName':SNMPget(Addr, DR5000SvcName),
+            }
+    
+    for key in Info:
+        print(Info[key])
+        c.writerow(Info[key])
--- SNMPInfo/core.py
+++ SNMPInfo/core.py
@@ -10,13 +10,14 @@ Script de relevé des niveaux de réceptions des IRD nodal
 import time
 import logging
 import configparser
+import csv
 from logging.handlers import RotatingFileHandler
 from argparse import ArgumentParser
-from Libraries import PrintException
+from Libraries import PrintException, DR5000state
 
 # Activation du logger principal
 try:
-    handler = RotatingFileHandler('/var/log/SNMPInfo.log', maxBytes=10000000, backupCount=5)
+    handler = RotatingFileHandler('D:\SNMPInfo.log', maxBytes=10000000, backupCount=5)
     handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
     logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
     logger = logging.getLogger(__name__)
@@ -35,11 +36,11 @@ try:
     config = configparser.ConfigParser()
     config.read(args.config)
     DR5000Addr = config.get('DR5000','IPAddr')
+    DR5000Model = config.get('DR5000','Model')
     DR5000Name = config.get('DR5000','OidName')
-    DR5000Model = config.get('DR5000','OidModel')
-    DR5000Power = config.get('DR5000','OidPower')
+    DR5000Snr = config.get('DR5000','OidSnr')
     DR5000Margin = config.get('DR5000','OidMargin')
-    DR50000SvcName = config.get('DR5000','OidServiceName')
+    DR5000SvcName = config.get('DR5000','OidServiceName')
 
 except:
     PrintException("Fichier de configuration invalide ou non précisé.\n\033[1;31mPour rappel :\033[1;33m sudo ./core.py -c 'emplacement du fichier de configuration'\033[0m")
@@ -47,4 +48,7 @@ except:
 
 # Démarrage de la boucle de vérification d'état de transmission
 logger.info("Initialisation du script...")
-time.sleep(2)
+c = csv.writer(open("D:\\fichier_status.csv", "w"))
+
+for Addr in DR5000Addr.split(","):
+    DR5000state(Addr, DR5000Model, DR5000Name, DR5000Snr, DR5000Margin, DR5000SvcName, c)
