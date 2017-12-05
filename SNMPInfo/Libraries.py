725a5639d4d63ff9f861119529f6d1b495a16f50
Vincent <31508339+vbnin@users.noreply.github.com>
2017-12-05 23:07:39 +0100

    Changements

--- SNMPInfo/Libraries.py
+++ SNMPInfo/Libraries.py
@@ -10,11 +10,13 @@ Ce fichier est une librairie requise par le script core.py
 import logging
 import re
 import csv
+import subprocess
+import platform
 from logging.handlers import RotatingFileHandler
 from pysnmp.hlapi import *
 
 # Activation du logger
-handler = RotatingFileHandler('D:\SNMPInfo.log', maxBytes=10000000, backupCount=5)
+handler = RotatingFileHandler('SNMPInfo.log', maxBytes=10000000, backupCount=5)
 handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
 logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
 logger = logging.getLogger(__name__)
@@ -26,7 +28,23 @@ def PrintException(msg):
     print(msg)
     print("***********************************************************************")
 
-# Définition de la commande SNMP Get
+# Définition de la fonction de vérification du type d'OS utilisé
+def OSType(OS):
+    if platform.system().lower() == OS:
+        return True
+    else:
+        return False
+
+# Définition de la fonction de ping
+def Ping(hostname, OS):
+    try:
+        subprocess.check_output("ping -{0} 2 -w 300 -{1} 255 {2}".format("n" if OSType(OS) is True else "c", "i" if OSType(OS) is True else "t", hostname))
+    except:
+        logger.error("L'IRD " + hostname + " est injoignable !!")
+        return False
+    return True
+
+# Définition de la commande 'SNMP Get'
 def SNMPget(IPAddr, OID):
     try:
         errorIndication, errorStatus, errorIndex, varBinds = next(
@@ -35,7 +53,6 @@ def SNMPget(IPAddr, OID):
                 UdpTransportTarget((IPAddr, 161)),
                 ContextData(),
                 ObjectType(ObjectIdentity(OID))))
-
         if errorIndication:
             logger.error(errorIndication)
             state = '2'
@@ -57,21 +74,58 @@ def SNMPget(IPAddr, OID):
         state = '2'
         return state
 
-def DR5000state(Position, DR5000Name, Addr, DR5000SvcName, DR5000Snr, DR5000Margin, c):
-    DataCSV = []
+def DR5000state(Position, Name, Addr, SvcName, Snr, Margin, f, DataCSV):
     Info = {'Position':Position,
-            'Name':SNMPget(Addr, DR5000Name),
+            'Name':SNMPget(Addr, Name),
             'Addr':Addr,
             'Model':"Ateme DR5000",
-            'SvcName':SNMPget(Addr, DR5000SvcName),
-            'Snr':int(SNMPget(Addr, DR5000Snr))/10,
-            'Margin':int(SNMPget(Addr, DR5000Margin))/10,
+            'SvcName':SNMPget(Addr, SvcName),
+            'Snr':int(SNMPget(Addr, Snr))/10,
+            'Margin':int(SNMPget(Addr, Margin))/10,
             }
-    
     for key in Info:
         DataCSV.append(Info[key])
+    logger.debug(DataCSV)
+    f.writerow(DataCSV)
 
-    print(DataCSV)
-    c.writerow(DataCSV)
+def RX8200state(Position, Name, Addr, SvcName, Snr, Margin, f, DataCSV):
+    Info = {'Position':Position,
+            'Name':SNMPget(Addr, Name),
+            'Addr':Addr,
+            'Model':"Ericson RX8200",
+            'SvcName':SNMPget(Addr, SvcName),
+            'Snr':int(SNMPget(Addr, Snr)),
+            'Margin':int(SNMPget(Addr, Margin)),
+            }
+    for key in Info:
+        DataCSV.append(Info[key])
+    logger.debug(DataCSV)
+    f.writerow(DataCSV)
 
-# Dupliquer DR5000state et personnaliser par IRD
+def TT1260state(Position, Name, Addr, SvcName, Snr, Margin, f, DataCSV):
+    Info = {'Position':Position,
+            'Name':SNMPget(Addr, Name),
+            'Addr':Addr,
+            'Model':"Tandberg TT1260",
+            'SvcName':SNMPget(Addr, SvcName),
+            'Snr':int(SNMPget(Addr, Snr)),
+            'Margin':int(SNMPget(Addr, Margin)),
+            }
+    for key in Info:
+        DataCSV.append(Info[key])
+    logger.debug(DataCSV)
+    f.writerow(DataCSV)
+
+def RX1290state(Position, Name, Addr, SvcName, Snr, Margin, csv, DataCSV):
+    Info = {'Position':Position,
+            'Name':SNMPget(Addr, Name),
+            'Addr':Addr,
+            'Model':"Ericson RX1290",
+            'SvcName':SNMPget(Addr, SvcName),
+            'Snr':int(SNMPget(Addr, Snr)),
+            'Margin':int(SNMPget(Addr, Margin)),
+            }
+    for key in Info:
+        DataCSV.append(Info[key])
+    logger.debug(DataCSV)
+    f.writerow(DataCSV)
--- SNMPInfo/core.py
+++ SNMPInfo/core.py
@@ -13,11 +13,11 @@ import configparser
 import csv
 from logging.handlers import RotatingFileHandler
 from argparse import ArgumentParser
-from Libraries import PrintException, DR5000state
+from Libraries import PrintException, DR5000state, RX8200state, TT1260state, RX1290state, Ping
 
 # Activation du logger principal
 try:
-    handler = RotatingFileHandler('D:\SNMPInfo.log', maxBytes=10000000, backupCount=5)
+    handler = RotatingFileHandler('SNMPInfo.log', maxBytes=10000000, backupCount=5)
     handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
     logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
     logger = logging.getLogger(__name__)
@@ -33,30 +33,31 @@ args = parser.parse_args()
 
 # Lecture du fichier de Configuration et attribution des variables
 try:
+    ird = DR5000 = RX8200 = TT1260 = RX1290 = {}
     config = configparser.ConfigParser()
     config.read(args.config)
-    DR5000Name = config.get('DR5000', 'OidName')
-    DR5000Snr = config.get('DR5000', 'OidSnr')
-    DR5000Margin = config.get('DR5000', 'OidMargin')
-    DR5000SvcName = config.get('DR5000', 'OidServiceName')
-    RX8200Name = config.get('RX8200', 'OidName')
-    RX8200Snr = config.get('RX8200', 'OidSnr')
-    RX8200Margin = config.get('RX8200', 'OidMargin')
-    RX8200SvcName = config.get('RX8200', 'OidServiceName')
-    TT1260Name = config.get('TT1260', 'OidName')
-    TT1260Snr = config.get('TT1260', 'OidSnr')
-    TT1260Margin = config.get('TT1260', 'OidMargin')
-    TT1260SvcName = config.get('TT1260', 'OidServiceName')
-    RX1290Name = config.get('RX1290', 'OidName')
-    RX1290Snr = config.get('RX1290', 'OidSnr')
-    RX1290Margin = config.get('RX1290', 'OidMargin')
-    RX1290SvcName = config.get('RX1290', 'OidServiceName')
-    s = {}
+    OS = config.get('GENERAL', 'OSType')
+    DR5000['Name'] = config.get('DR5000', 'OidName')
+    DR5000['Snr'] = config.get('DR5000', 'OidSnr')
+    DR5000['Margin'] = config.get('DR5000', 'OidMargin')
+    DR5000['SvcName'] = config.get('DR5000', 'OidServiceName')
+    RX8200['Name'] = config.get('RX8200', 'OidName')
+    RX8200['Snr'] = config.get('RX8200', 'OidSnr')
+    RX8200['Margin'] = config.get('RX8200', 'OidMargin')
+    RX8200['SvcName'] = config.get('RX8200', 'OidServiceName')
+    TT1260['Name'] = config.get('TT1260', 'OidName')
+    TT1260['Snr'] = config.get('TT1260', 'OidSnr')
+    TT1260['Margin'] = config.get('TT1260', 'OidMargin')
+    TT1260['SvcName'] = config.get('TT1260', 'OidServiceName')
+    RX1290['Name'] = config.get('RX1290', 'OidName')
+    RX1290['Snr'] = config.get('RX1290', 'OidSnr')
+    RX1290['Margin'] = config.get('RX1290', 'OidMargin')
+    RX1290['SvcName'] = config.get('RX1290', 'OidServiceName')
     for i in range(1, 36):
-        Name = "ird" + str(i)
+        Position = "ird" + str(i)
         Model = "type" + str(i)
-        s[Name] = config.get('IRD', 'IRD' + str(i))
-        s[Model] = config.get('IRD', 'IRD' + str(i) + 'Model')
+        ird[Position] = config.get('IRD', 'IRD' + str(i))
+        ird[Model] = config.get('IRD', 'IRD' + str(i) + 'Model')
 
 except:
     PrintException("Fichier de configuration invalide ou non précisé.Pour rappel : sudo ./core.py -c 'emplacement du fichier de configuration'")
@@ -66,19 +67,19 @@ except:
 logger.info("Initialisation du script...")
 
 while True:
-    c = csv.writer(open("D:\\fichier_status.csv", "w", newline=''), delimiter = ';')
+    DataCSV = []
+    f = csv.writer(open("fichier_status.csv", "w", newline=''), delimiter = ';')
     for i in range(1, 36):
-        Name = "ird" + str(i)
+        Position = "ird" + str(i)
         Model = "type" + str(i)
-        if s[Model] == 'DR5000':
-            DR5000state(Name, DR5000Name, s[Name], DR5000SvcName, DR5000Snr, DR5000Margin, c)
-        elif s[Model] == 'RX8200':
-            print('RX8200state')
-        elif s[Model] == 'TT1260':
-            print('TT1260state')
-        elif s[Model] == 'RX1290':
-            print('RX1290state')
+        if ird[Model] == 'DR5000' and Ping(ird[Position], OS) is True:
+            DR5000state(Position, DR5000['Name'], ird[Position], DR5000['SvcName'], DR5000['Snr'], DR5000['Margin'], f, DataCSV)
+        elif ird[Model] == 'RX8200' and Ping(ird[Position], OS) is True:
+            RX8200state(Position, RX8200['Name'], ird[Position], RX8200['SvcName'], RX8200['Snr'], RX8200['Margin'], f, DataCSV)
+        elif ird[Model] == 'TT1260' and Ping(ird[Position], OS) is True:
+            TT1260state(Position, TT1260['Name'], ird[Position], TT1260['SvcName'], TT1260['Snr'], TT1260['Margin'], f, DataCSV)
+        elif ird[Model] == 'RX1290' and Ping(ird[Position], OS) is True:
+            RX1290state(Position, RX1290['Name'], ird[Position], RX1290['SvcName'], RX1290['Snr'], RX1290['Margin'], f, DataCSV)
         else:
-            pass
+            logger.error("Erreur à la position " + str(i))
     time.sleep(1)
-# Définir les fonctions pour chaque cas
