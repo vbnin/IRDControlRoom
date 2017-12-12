324e04be040e954ce8ff28793233543054bebe3d
vbnin <31508339+vbnin@users.noreply.github.com>
2017-12-12 15:10:34 +0100

    Changes in a good way

--- SNMPReceiver/Libraries.py
+++ SNMPReceiver/Libraries.py
@@ -6,10 +6,9 @@ Developeurs : VBNIN + CKAR - IPEchanges.
 Ce fichier est une librairie requise par le script core.py
 """
 
-# Import des librairies
-import logging
 import re
 import csv
+import logging
 import threading
 import subprocess
 from logging.handlers import RotatingFileHandler
@@ -27,70 +26,60 @@ logger.addHandler(handler)
 
 # Définition de la fonction PrintException
 def PrintException(msg):
-    print("*" * 30)
+    print("*" * 40)
     print(msg)
-    print("*" * 30)
+    print("*" * 40)
 
+# Définition de la fonction de remplissage du fichier CSV au démarrage
 def Launcher(Data):
     DataCSV = []
-    Threads = []
-    t = {}
-    f = csv.writer(open("fichier_status.csv", "w", newline=''), delimiter = ';')
     for i in range(1, 6):
         Position = "ird" + str(i)
         Model = "type" + str(i)
         SatName = "SAT-" + str(i)
         if Data[Model] == 'DR5000':
-            t[str(i)] = threading.Thread(target=DR5000state, args=(Position, SatName, Data, DataCSV))
-            t[str(i)].start()
-            Threads.append(t[str(i)])
-        # elif Data[Model] == 'RX8200':
-        #     t[str(i)] = threading.Thread(target=RX8200state, args=(Position, SatName, Data[Position], RX8200['SvcName'], RX8200['Snr'], RX8200['Margin'], DataCSV))
-        #     t[str(i)].start()
-        #     Threads.append(t[str(i)])
-        # elif Data[Model] == 'TT1260':
-        #     t[str(i)] = threading.Thread(target=TT1260state, args=(Position, SatName, Data[Position], TT1260['SvcName'], TT1260['Snr'], TT1260['Margin'], DataCSV))
-        #     t[str(i)].start()
-        #     Threads.append(t[str(i)])
+            DR5000state(Position, SatName, Data[Position], Data, DataCSV)
+        elif Data[Model] == 'RX8200':
+            RX8200state(Position, SatName, Data[Position], Data, DataCSV)
+        elif Data[Model] == 'TT1260':
+            TT1260state(Position, SatName, Data[Position], Data, DataCSV)
         else:
-            NoSat(Position, SatName, Data[Model], DataCSV)
-    for t in Threads:
-        try:
-            t.join()
-        except:
-            logger.error("Thread already finished")     
-    f.writerows(DataCSV)
-    logger.info("Done")
+            NoSat(Position, SatName, Data[Position], Data[Model], DataCSV, Data)
+    with open("fichier_status.csv", "w", newline='') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerows(DataCSV)
 
 # Fonction de récupération de l'état IRD
 def IRDstate(Addr, Model, Data):
-    logger.info("Sending new Query...")
-    #
-    #Faire un each pour chercher la bonne valeur dans Data
-    #
-    if Addr == Data["ird1"]:
-    ird = 'ird ' + str(Addr[2:])
-    sat = 'Sat ' + str(Addr[2:])
-    d = []
-    if Model == 'DR5000':
-        Info = {'Position':ird,
-            'Name':sat,
-            'Addr':Addr,
-            'Model':"Ateme DR5000",
-            'SvcName':SNMPget(Addr, Data['DR5000SvcName']),
-            'Snr':int(SNMPget(Addr, Data['DR5000Snr']))/10,
-            'Margin':int(SNMPget(Addr, Data['DR5000Margin']))/10,
-            }
-    for key in Info:
-        d.append(Info[key])
-    f = csv.reader(open("fichier_status.csv"), delimiter=';')
-    lines = [l for l in f]
-
-#
-# Réécrire CSV !
-#
-    
-    # elif Model == 'TT1260':
+    DataCSV = []
+    logger.info("Envoi d'une requete d'état sur le port 161.")
+    for i in range(1, 36):
+        Position = "ird" + str(i)
+        SatName = "SAT-" + str(i)
+        if Addr == Data[Position]:
+            if Model == 'DR5000':
+                DR5000state(Position, SatName, Addr, Data, DataCSV)
+            elif Model == 'RX8200':
+                RX8200state(Position, SatName, Addr, Data, DataCSV)
+            elif Model == 'TT1260':
+                TT1260state(Position, SatName, Addr, Data, DataCSV)
+            else:
+                return
+            with open("fichier_status.csv") as f:
+                reader = csv.reader(f, delimiter=';')
+                lines = [l for l in reader]
+                for item in lines:
+                    try:
+                        item.index(Position)
+                        lines.remove(item)
+                        lines.append(DataCSV[0])
+                    except:
+                        pass
+            with open("fichier_status.csv", "w", newline='') as f:
+                writer = csv.writer(f, delimiter=';')
+                writer.writerows(lines)
+        else:
+            pass
 
 # Définition de la commande 'SNMP Get'
 def SNMPget(IPAddr, OID):
@@ -98,39 +87,33 @@ def SNMPget(IPAddr, OID):
         errorIndication, errorStatus, errorIndex, varBinds = next(
             getCmd(SnmpEngine(),
                 CommunityData('private', mpModel=0),
-                UdpTransportTarget((IPAddr, 161)),
+                UdpTransportTarget((IPAddr, 161), timeout=0.1),
                 ContextData(),
                 ObjectType(ObjectIdentity(OID))))
-        if errorIndication:
-            logger.error(errorIndication)
-            state = 'Erreur'
-            return state
-        elif errorStatus:
-            logger.error('%s at %s' % (errorStatus.prettyPrint(),
-                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-            state = 'Erreur'
+        if errorIndication or errorStatus:
+            logger.error("No SNMP response before timeout")
+            state = '0'
             return state
         else:
             for varBind in varBinds:
                 state = (' = '.join([x.prettyPrint() for x in varBind]))
-                logger.debug(state)
                 m = re.search('(.*)\ =\ (.*)', state)
                 state = m.group(2)
                 return state
     except:
         logger.error("Impossible de récupérer les infos SNMP...")
-        state = 'Erreur'
-        return state    
+        state = '0'
+        return state
 
-def DR5000state(Position, SatName, Data, DataCSV):
+def DR5000state(Position, SatName, Addr, Data, DataCSV):
     Info = {'Position':Position,
             'Name':SatName,
-            'Addr':Data[Position],
+            'Addr':Addr,
             'Model':"Ateme DR5000",
-            'SvcName':SNMPget(Data[Position], Data['DR5000SvcName']),
-            'Snr':SNMPget(Data[Position], Data['DR5000Snr']),
-            'Margin':SNMPget(Data[Position], Data['DR5000Margin']),
-            }
+            'SvcName':SNMPget(Addr, Data['DR5000SvcName']),
+            'Snr':int(SNMPget(Addr, Data['DR5000Snr']))/10,
+            'Margin':int(SNMPget(Addr, Data['DR5000Margin']))/10,
+           }
     d = []
     for key in Info:
         d.append(Info[key])
@@ -138,51 +121,17 @@ def DR5000state(Position, SatName, Data, DataCSV):
     DataCSV.append(d)
     return DataCSV
 
-# def RX8200state(Position, Name, Addr, SvcName, Snr, Margin, DataCSV):
-#     Info = {'Position':Position,
-#             'Name':Name,
-#             'Addr':Addr,
-#             'Model':"Ericsson RX8200",
-#             'SvcName':SNMPget(Addr, SvcName),
-#             'Snr':SNMPget(Addr, Snr)[:4],
-#             'Margin':SNMPget(Addr, Margin)[2:6],
-#             }
-#     if Info['SvcName'][:7] == "No Such":
-#         Info['SvcName'] = ''
-#     d = []
-#     for key in Info:
-#         d.append(Info[key])
-#     print(d)
-#     DataCSV.append(d)
-#     return DataCSV
-
-# def TT1260state(Position, Name, Addr, SvcName, Snr, Margin, DataCSV):
-#     Info = {'Position':Position,
-#             'Name':Name,
-#             'Addr':Addr,
-#             'Model':"Tandberg TT1260",
-#             'SvcName':SNMPget(Addr, SvcName),
-#             'Snr':int(SNMPget(Addr, Snr))/100,
-#             'Margin':int(SNMPget(Addr, Margin))/100,
-#             }
-#     if Info['Margin'] == 100.0:
-#         Info['Margin'] = 0.0
-#     d = []
-#     for key in Info:
-#         d.append(Info[key])
-#     print(d)
-#     DataCSV.append(d)
-#     return DataCSV
-
-def NoSat(Position, SatName, Model, DataCSV):
+def RX8200state(Position, SatName, Addr, Data, DataCSV):
     Info = {'Position':Position,
             'Name':SatName,
-            'Addr':'',
-            'Model':Model,
-            'SvcName':'',
-            'Snr':'',
-            'Margin':'',
-            }
+            'Addr':Addr,
+            'Model':"Ericsson RX8200",
+            'SvcName':SNMPget(Addr, Data['RX8200SvcName']),
+            'Snr':SNMPget(Addr, Data['RX8200Snr'])[:4],
+            'Margin':SNMPget(Addr, Data['RX8200Margin'])[2:6],
+           }
+    if Info['SvcName'][:7] == "No Such":
+        Info['SvcName'] = ''
     d = []
     for key in Info:
         d.append(Info[key])
@@ -190,3 +139,36 @@ def NoSat(Position, SatName, Model, DataCSV):
     DataCSV.append(d)
     return DataCSV
 
+def TT1260state(Position, SatName, Addr, Data, DataCSV):
+    Info = {'Position':Position,
+            'Name':SatName,
+            'Addr':Addr,
+            'Model':"Tandberg TT1260",
+            'SvcName':SNMPget(Addr, Data['TT1260SvcName']),
+            'Snr':int(SNMPget(Addr, Data['TT1260Snr']))/100,
+            'Margin':int(SNMPget(Addr, Data['TT1260Margin']))/100,
+           }
+    if Info['Margin'] == 100.0:
+        Info['Margin'] = 0.0
+    d = []
+    for key in Info:
+        d.append(Info[key])
+    logger.info(d)
+    DataCSV.append(d)
+    return DataCSV
+
+def NoSat(Position, SatName, Addr, Model, DataCSV, Data):
+    Info = {'Position':Position,
+            'Name':SatName,
+            'Addr':Addr,
+            'Model':Model,
+            'SvcName':'N/A',
+            'Snr':'N/A',
+            'Margin':'N/A',
+           }
+    d = []
+    for key in Info:
+        d.append(Info[key])
+    logger.info(d)
+    DataCSV.append(d)
+    return DataCSV
--- SNMPReceiver/core.py
+++ SNMPReceiver/core.py
@@ -6,16 +6,15 @@ Developeurs : VBNIN + CKAR - IPEchanges.
 Script de relevé des niveaux de réceptions des IRD nodal
 """
 
-from pysnmp.carrier.asyncore.dispatch import AsyncoreDispatcher
-from pysnmp.carrier.asyncore.dgram import udp
-from pyasn1.codec.ber import decoder
-from pysnmp.proto import api
-from logging.handlers import RotatingFileHandler
-from Libraries import PrintException, IRDstate, Launcher
 import logging
-from argparse import ArgumentParser
 import configparser
-import csv
+from pysnmp.proto import api
+from pyasn1.codec.ber import decoder
+from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.carrier.asyncore.dispatch import AsyncoreDispatcher
+from logging.handlers import RotatingFileHandler
+from argparse import ArgumentParser
+from Libraries import PrintException, IRDstate, Launcher
 
 # Activation du logger principal
 try:
@@ -30,7 +29,7 @@ except:
 
 # Récupération des variables de démarrage
 parser = ArgumentParser()
-parser.add_argument("-c", "--config", dest="config", help="Préciser le chemin du fichier config.ini")
+parser.add_argument("-c", "--config", dest="config", help="Fichier config.ini")
 args = parser.parse_args()
 
 # Lecture du fichier de Configuration et attribution des variables
@@ -57,49 +56,41 @@ try:
         Data[Position] = config.get('IRD', 'IRD' + str(i))
         Data[Model] = config.get('IRD', 'IRD' + str(i) + 'Model')
 except:
-    PrintException("Fichier de configuration invalide ou non précisé.Pour rappel : core.py -c 'emplacement du fichier de configuration'")
+    PrintException("Fichier de configuration invalide ou non précisé. "
+                   "Pour rappel : core.py -c config.ini")
     exit()
 
 # CallBack function for receiving traps
 def cbFun(transportDispatcher, transportDomain, transportAddress, wholeMsg):
-    trap = {}
     global Addresses
     global Data
     while wholeMsg:
         msgVer = int(api.decodeMessageVersion(wholeMsg))
         pMod = api.protoModules[msgVer]
         reqMsg, wholeMsg = decoder.decode(
-            wholeMsg, asn1Spec=pMod.Message(),
-        )
-        logger.info('Notification message from ' + str(transportAddress[0]))
-        trap['Addr'] = transportAddress[0]
-        reqPDU = pMod.apiMessage.getPDU(reqMsg)
-        if reqPDU.isSameTypeWith(pMod.TrapPDU()):
-            varBinds = pMod.apiPDU.getVarBinds(reqPDU)
-            for oid, val in varBinds:
-                trap[oid.prettyPrint()] = val.prettyPrint()
-        if trap['Addr'] in Addresses['DR5000']:
-            IRDstate(trap['Addr'], 'DR5000', Data)
-        elif trap['Addr'] in Addresses['TT1260']:
-            IRDstate(trap['Addr'], 'TT1260', Data)
-        elif trap['Addr'] in Addresses['RX8200']:
-            IRDstate(trap['Addr'], 'RX8200', Data)
-    return trap  
+            wholeMsg, asn1Spec=pMod.Message())
+        logger.info('Trap SNMP recu de : ' + str(transportAddress[0]))
+        if transportAddress[0] in Addresses['DR5000']:
+            IRDstate(transportAddress[0], 'DR5000', Data)
+        elif transportAddress[0] in Addresses['TT1260']:
+            IRDstate(transportAddress[0], 'TT1260', Data)
+        elif transportAddress[0] in Addresses['RX8200']:
+            IRDstate(transportAddress[0], 'RX8200', Data)
 
 # Ouverture du socket IPv4 SNMP
 transportDispatcher = AsyncoreDispatcher()
 transportDispatcher.registerRecvCbFun(cbFun)
 transportDispatcher.registerTransport(
-    udp.domainName, udp.UdpSocketTransport().openServerMode(('0.0.0.0', 162))
-)
+    udp.domainName, udp.UdpSocketTransport().openServerMode(('0.0.0.0', 162)))
 transportDispatcher.jobStarted(1)
 
+# Dispatcher will never finish as job#1 never reaches zero
 try:
-    # Dispatcher will never finish as job#1 never reaches zero
-    #f = csv.writer(open("fichier_status.csv", "a", newline=''), delimiter = ';')
     logger.info("Initialisation du script...")
     Launcher(Data)
+    logger.info("Initialisation terminée, écoute des traps sur le port 162...")
     transportDispatcher.runDispatcher()
 except:
     transportDispatcher.closeDispatcher()
+    logger.info("Fin du script.")
     raise
