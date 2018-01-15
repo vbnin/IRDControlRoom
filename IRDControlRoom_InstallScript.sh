55520738eee5166cfad7cfa1a453e56c51220434
vbnin <31508339+vbnin@users.noreply.github.com>
2018-01-15 14:12:10 +0100

    Modif du script d'installation

--- IRDControlRoom_InstallScript.sh
+++ IRDControlRoom_InstallScript.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 echo "*** Déplacement du dossier dans /usr/local/bin ***"
-cd /home/nodal/
-sudo mv -f IRDControlRoom/ /usr/local/bin/
+DIRECTORY=$(cd `dirname $0` && pwd)
+sudo chown nodal nodal $DIRECTORY/SNMPReceiver/*
+sudo mv -f $DIRECTORY /usr/local/bin/
 
 echo "*** Ajout des droits d'exécution ***"
 cd /usr/local/bin/IRDControlRoom/SNMPReceiver/
+sudo chown nodal nodal *
 sudo chmod +x core.py
 sudo chmod +x libraries.py
 
 echo "*** Activation du script au reboot via sudo crontab ***"
 echo -e "$(crontab -u root -l)\n@reboot /usr/bin/python3 /usr/local/bin/IRDControlRoom/SNMPReceiver/core.py | crontab -u root -l
 
-echo "*** Installation des packages Python3 pré-requis ***"
+echo "*** Installation des packages Python3 requis ***"
+sudo apt-get install python3-pip
 sudo pip3 install configparser
 sudo pip3 install pysnmp
 
