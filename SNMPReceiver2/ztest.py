04cd0882ea3fefc3fb60d43ad631eb3fdb1efec9
vbnin <vincent.bonnin@outlook.com>
2018-02-07 16:17:05 +0100

    Modif Release 2 avec easySNMP

---
+++ SNMPReceiver2/ztest.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Developeurs : VBNIN + CKAR - IPEchanges.
+Script de relevé des niveaux de réceptions des IRD nodal
+"""
+#Librairies externe à télécharger
+from pysnmp.hlapi import *
+from pysnmp.carrier.asyncore.dgram import udp, unix
+from pyasn1.codec.ber import decoder
+from pysnmp.proto import api
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+from easysnmp import Session
+
+#Import des librairies internes
+import sys
+import csv
+import re
+import socket
+import logging
+import time
+from logging.handlers import RotatingFileHandler
+
+
+# Activation du logger
+LogPath = 'SNMPReceiver.log' if sys.platform.lower() == 'win32' else '/var/log/SNMPReceiver.log'
+handler = RotatingFileHandler(LogPath, maxBytes=10000000, backupCount=5)
+handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
+logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+
+OID1 = '.1.3.6.1.4.1.27338.5.5.3.3.2.0'
+OID2 = '.1.3.6.1.4.1.27338.5.5.3.3.3.0'
+OID3 = '.1.3.6.1.4.1.27338.5.5.1.5.1.1.9.1'
+OID4 = '.1.3.6.1.4.1.27338.5.5.3.3'
+
+# Définition de la commande 'SNMP Get'
+def SNMPget(IPAddr, SNMPv, OID1, OID2, OID3):
+    try:
+        snmp = []
+        OIDs = [OID1, OID2, OID3]
+        session = Session(hostname=IPAddr, community='private', version=SNMPv)
+        for item in OIDs:
+            m = re.search(r'(value=)\'(.*)\'\ (.*)', session.get(item))
+            snmp.append(m.group(2))
+        logger.info(snmp)
+        return snmp
+    except Exception:
+        logger.error(Exception)
+        snmp = ['Erreur : SNMP timeout', 0.0, 0.0]
+        return snmp
+
+def EasySNMP(OID1, OID2, OID3):
+    # Create an SNMP session to be used for all our requests
+    session = Session(hostname='10.75.216.226', community='private', version=2)
+    print(session.get(OID1))
+    print(session.get(OID2))
+    print(session.get(OID3))
+
+# # Test avec Librairie EasySNMP
+# start = time.time()
+# for i in range(0,10):
+#     EasySNMP(OID1, OID2, OID3)
+# stop = time.time()-start
+# logger.info(stop)
+
+# Test avec pysnmp en version Get
+start = time.time()
+for i in range(0,10):
+    SNMPget('10.75.216.4', 2, OID1, OID2, OID3)
+stop = time.time()-start
+logger.info(stop)
+
+# # Test avec pysnmp en version Bulk
+# start = time.time()
+# for i in range(0,10):
+#     SNMPbulk(OID4)
+# stop = time.time()-start
+# logger.info(stop)
+
+
